f5e0bb739f8b68211c74dba71d5f9e2279405de1
benjamingarciap <bengarc@gmail.com>
2019-03-26 22:49:16 -0500

    Second Commit - Add Second rover. Add otherRover Obstacle conditions beta.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,23 +1,37 @@
-// Rover Object Goes Here
+// Rover Objects Go Here
 // ======================
-var rover = {
+var rover1 = {
   direction:"N",
   x:0, 
   y:0,
-  travelLog:[]
+  travelLog:[],
+  position:[]
 };
+var rover2 = {
+  direction:"N",
+  x:1, 
+  y:0,
+  travelLog:[],
+  position: []
+};
+var otherRover;
+// ======================
+
+// Turn variable goes here
+// ======================
+var turn = 1;
 // ======================
 
 // Grid goes here
 // ======================
 var grid = [
-  [null, null, null, "O", null, null, null, null, null, null],
-  [null, "O", null, "O", null, null, null, null, null, null],
-  ["O", null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, "O", null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, "O", null, null, null, null],
   [null, "O", null, null, null, null, null, null, null, null],
-  [null, null, null, null, "O", null, null, null, null, null],
-  [null, null, null, null, "O", null, null, null, null, null],
-  [null, null, null, null, "O", null, null, null, null, null],
+  [null, null, null, null, "O", null, null, "O", null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, "O", null, null, "O", null, null],
   [null, null, null, null, "O", null, null, null, null, null],
   [null, null, null, null, "O", null, null, null, null, null],
   [null, null, null, null, "O", null, null, null, null, null],
@@ -36,6 +50,7 @@ function turnLeft(rover){
     case "S": rover.direction = "E";
     break;
     case "E": rover.direction = "N";
+    break;
     default: console.log("Does not compute");
     break;
   }
@@ -59,16 +74,51 @@ function turnRight(rover){
 }
 
 function moveForward(rover){
-  // Bounderies conditions
+  // Rover definition
+  switch(rover){
+    case rover1: otherRover = rover2;
+    break;
+    case rover2: otherRover = rover1; 
+    break;
+  }
+  // Rovers position definition
+  rover["position"].push(rover.y, rover.x);
+  otherRover["position"].push(otherRover["y"], otherRover["x"]);
+  // Grid boundaries conditions
   if(rover.x === 0 && rover.direction === "W"|| rover.y === 0 && rover.direction === "N" 
   || rover.x === 10 && rover.direction === "E" || rover.y === 10 && rover.direction === "S"){
-    return "Rover stoped. Can´t go over the grid bounderies"
+    rover["position"] = [];
+    otherRover["position"] = [];
+    return "Rover stoped. Can´t go over the grid bounderies";
   } 
-  // Obstacle conditions
-  else if (grid[rover.y][rover.x + 1] === "O" && rover.direction === "E" || grid[rover.y + 1][rover.x] === "O" && rover.direction === "S"
-  || grid[rover.y][rover.x - 1] === "O" && rover.direction === "W" || grid[rover.y - 1][rover.x] === "O" && rover.direction === "N") {
-    return "Rover stoped. Can´t move trough grid obstacle"
+  // Grid obstacle conditions
+  else if(grid[rover.y][rover.x + 1] === "O" && rover.direction === "E" || grid[rover.y + 1][rover.x] === "O" && rover.direction === "S"
+  || grid[rover.y][rover.x - 1] === "O" && rover.direction === "W" /* || grid[rover.y - 1][rover.x] === "O" && rover.direction === "N" */) {
+    rover["position"] = [];
+    otherRover["position"] = [];
+    return "Rover stoped. Can´t move trough grid obstacle";
   }
+  // otherRover Obstacle conditions
+  else if(rover["position"][0] === otherRover["position"][0] && rover["position"][1] + 1  === otherRover["position"][1] && rover.direction === "E" 
+    || rover["position"][0] + 1 === otherRover["position"][0] && rover["position"][1] === otherRover["position"][1] && rover.direction === "S" 
+    || rover["position"][0] === otherRover["position"][0] && rover["position"][1] - 1  === otherRover["position"][1] && rover.direction === "W"
+    || rover["position"][0] - 1 === otherRover["position"][0] && rover["position"][1]  === otherRover["position"][1] && rover.direction === "N"){
+     // Rovers position reset
+    rover["position"] = [];
+    otherRover["position"] = [];
+    return "Rover stoped. Can´t move " + otherRover + " blocking path";
+  }
+ 
+  
+  /*
+  else if(grid[rover.y][rover.x + 1] === grid[otherRover2.y][otherRover2.x] && rover.direction === "E" || 
+  grid[rover.y + 1][rover.x] === grid[otherRover2.y][otherRover2.x] && rover.direction === "S"|| 
+  grid[rover.y][rover.x - 1] === grid[otherRover2.y][otherRover2.x] && rover.direction === "W" || 
+  grid[rover.y - 1][rover.x] === grid[otherRover2.y][otherRover2.x] && rover.direction === "N"){
+    return "Rover stoped. Can´t move " + otherRover2 + " blocking path"
+  }
+  */
+
   // TravelLog 
   rover.travelLog.push("y:" + rover.y + " x:" + rover.x);
   // Movement logic
@@ -84,7 +134,9 @@ function moveForward(rover){
     default: console.log("Does not compute");
     break;
 
-}
+  }
+  rover["position"] = [];
+  otherRover["position"] = [];
   console.log("moveForward was called. Mars Rover is on X:" + rover.x + " Y:" + rover.y);
 }
 
@@ -116,8 +168,13 @@ function moveBackwards(rover){
 }
   console.log("moveBackwards was called. Mars Rover is on X:" + rover.x + " Y:" + rover.y);
 }
+// ======================
 
-function freeMovement(commands){
+
+// Complex Movement Functions Go Here
+// ======================
+
+function freeMovement(rover ,commands){
   for(i=0; i<commands.length; i++){
     if(commands[i]=== "f"){
       moveForward(rover);
